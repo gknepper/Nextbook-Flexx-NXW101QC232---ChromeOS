a868c71d1c13f77422ddf817e71ca938ab4611a6
Gabriel Knepper Mendes <gabriel666@gmail.com>
2020-11-15 19:22:34 -0800

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# SUBSCRIBE to Kedar Nimbalkar on youtube for more such videos https://www.youtube.com/user/kedar123456889
+sudo apt-get update
+sudo apt-get install figlet
+sudo apt-get install pv
+sudo apt-get install cgpt
+sudo figlet -c "SUBSCRIBE TO"
+sudo figlet -c Kedar
+sudo figlet -c Nimbalkar
+sudo echo https://www.youtube.com/user/kedar123456889
+sudo bash chromeos-install.sh -src rammus_recovery.bin -dst /dev/mmcblk1
